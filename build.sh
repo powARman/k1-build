78aff114c4b64685228e0ab2ca17c3591c8cf526
Andreas Regel <andreas.regel@gmx.de>
2026-06-14 21:02:54 +0000

    Add build script for MCU firmware

---
+++ build.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+rm -r ./out/
+mkdir -p ./out/
+
+cd K1_Series_Klipper
+
+for filepath in src/configs/K1*; do
+
+    # Check if any files actually match the pattern
+    [ -e "$filepath" ] || continue
+
+    filename=$(basename "$filepath")
+    echo "------------------------------------------"
+    echo "Processing: $filename"
+    echo "------------------------------------------"
+
+    make $filename
+
+    echo "Running make clean..."
+    make clean
+
+    echo "Running make..."
+    make -j4
+
+    if [ -d "out" ] && ls out/*000.bin >/dev/null 2>&1; then
+        echo "Copying binaries..."
+        cp out/*000.bin ../out/
+    else
+        echo "Warning: 'out' directory not found or no .bin files exist."
+    fi
+done
+
+echo "Running make clean..."
+make clean
