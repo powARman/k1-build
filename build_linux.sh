e503487f3765ab1c8b4df903714c16f0dc6ba2e4
Andreas Regel <andreas.regel@gmx.de>
2026-07-19 15:29:49 +0200

    Add building of kernel modules

--- build_linux.sh
+++ build_linux.sh
@@ -5,3 +5,5 @@
 make -C "$KERNEL_SRC" O="$KERNEL_BUILD" x2000_k1_linux_defconfig
 
 make -j$(nproc) -C "$KERNEL_SRC" O="$KERNEL_BUILD" xImage
+
+make -j$(nproc) -C "$KERNEL_SRC" O="$KERNEL_BUILD" modules
